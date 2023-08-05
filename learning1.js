d3885125481918126aa33df45d65d0d40324b603
Alfath <alfath.pratama@finaccel.co>
2023-08-06 00:39:09 +0700

    learning1.js

---
+++ learning1.js
@@ -0,0 +1,19 @@
+let angka = [3, 5, 12];
+let sum = 0;
+for (let i = 0; i < angka.length; i++) {
+  sum += angka[i];
+}
+
+function loopAngka() {
+  let text = "";
+  for (let i = 0; i < angka.length; i++) {
+    if (i === angka.length - 1) {
+      text += angka[i];
+    } else {
+      text += angka[i] + "+";
+    }
+  }
+  return text;
+}
+
+console.log(`total nilai adalah ${sum} karena ${loopAngka(angka)}`);
